f09ecc666cec7371f6d266c292a17a62776d3098
habibsha <habibshahalahi7424@gmail.com>
2024-06-04 14:00:32 +0600

    Modify the navigation bar

--- js/courses.js
+++ js/courses.js
@@ -94,8 +94,7 @@ const loadCourses = () => {
 
   document.querySelectorAll(".details_btn").forEach((button) => {
     button.addEventListener("click", function () {
-      const courseId = Number(this.getAttribute("data-id"));
-      console.log("Course ID: ", typeof courseId);
+      const courseId = this.getAttribute("data-id");
       window.location.href = `/course-details.html?id=${courseId}`;
       console.log(window.location.href);
     });
